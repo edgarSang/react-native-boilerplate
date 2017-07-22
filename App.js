6f7eb321b8b6d9e83a1467bfb8c2be4583db25c2
KimJongMin <kdhx92@gmail.com>
2017-07-22 17:32:52 +0900

    Add loglevel library

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import log from 'loglevel';
 import Count from './src/components/Count';
 import { Provider } from 'react-redux';
 import store from './src/reducers/store';
+
+if (process.env.NODE_ENV === 'development') {
+  log.setLevel('trace', true);
+} else {
+  log.setLevel('error', true);
+}
 
 export default class App extends Component {
   render() {
