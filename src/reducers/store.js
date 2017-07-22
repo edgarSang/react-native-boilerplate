a59dace7396a429d57cb0404566305463580fc8c
KimJongMin <kdhx92@gmail.com>
2017-07-22 17:25:45 +0900

    Add redux-thunk middleware

--- src/reducers/store.js
+++ src/reducers/store.js
@@ -1,9 +1,11 @@
 import { applyMiddleware, compose, createStore } from 'redux';
 import reducer from './index';
 
+// Add middleware module
 import logger from './middleware/logger';
+import thunk from 'redux-thunk';
 
-const middlewares = [];
+const middlewares = [thunk];
 
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
